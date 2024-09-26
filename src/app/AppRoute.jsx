e0e18329b99e96ce4028f487065426d72c0a7327
Bagdat <clonn262@gmail.com>
2024-09-26 13:35:37 +0500

    fix

--- src/app/AppRoute.jsx
+++ src/app/AppRoute.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
-import "@/shared/styles/App.css";
 import { useTelegram } from "../shared/hooks/useTelegram";
 import { Header } from "../widgets/Header";
 import { Route, Routes } from "react-router-dom";
 import { ProductsList } from "../entities/ProductsList";
 import { Form } from "../features/Form";
+import "@/shared/styles/App.css";
 
 function App() {
   const { tg } = useTelegram();
@@ -18,7 +18,7 @@ function App() {
       <Header />
       <Routes>
         <Route index element={<ProductsList />} />
-        <Route path="/form" element={<Form />} />
+        <Route path={"form"} element={<Form />} />
       </Routes>
     </div>
   );
---
+++ src/widgets/ProductItem/index.js
@@ -0,0 +1,3 @@
+import ProductItem from "./ui/ProductItem"
+
+export { ProductItem }

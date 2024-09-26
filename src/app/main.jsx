443f4d325409045d1dac7a3e6094d8a657e0e0c3
Bagdat <clonn262@gmail.com>
2024-09-26 14:12:59 +0500

    fix

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./AppRoute";
 import "@/shared/styles/index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
